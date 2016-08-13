cbf7ce8b07990a68560a48abbecad142b7a5d911
Walker <jwalkerlyle@gmail.com>
2016-08-12 21:17:43 -0500

    still cannot get view

--- routes/items.js
+++ routes/items.js
@@ -1,6 +1,4 @@
-const http = require('http');
 const express = require('express');
-const path = require('path');
 const router = express.Router();
 const ItemModel = require('../models/ItemModel.js');
 
@@ -11,12 +9,7 @@ var item = new ItemModel({
 });
 
 router.get('/', function(req, res, next) {
-    // Here we are asking mongoose to find TaskModels,
-    // we are not passing any specifice attributes, such
-    // as an id, so mongoose will find all tasks.
   ItemModel.find((err, item) {
-    // Queries are run asynchronously.
-        // So we have to pass in a callback to be ran when the db query is finished
     res.render('index', {
       items: items
     });
@@ -26,9 +19,7 @@ router.get('/', function(req, res, next) {
 router.post('/', (req, res, next) => {
   const item = req.body.item;
   itemsArray.push(item);
-  model.save((err, item) => {
-        // Inserts are run asynchronously.
-        // So we have to pass in a callback to be ran when the insert is finished
+  item.save((err, item) => {
     res.redirect('/items');
   });
 });
